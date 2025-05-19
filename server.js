bdc239af3f3c138e10dafb90e31e6a49ffa3ffe1
skary-bio <pon4ikifree@gmail.com>
2025-05-19 18:56:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,37 @@ const io = require('socket.io')(http);
 // Обслуживаем статические файлы из папки public
 app.use(express.static('public'));
 
+// Массив для хранения истории рисования
+let drawHistory = [];
+
 io.on('connection', (socket) => {
     console.log('Пользователь подключился');
 
+    // Отправляем новому пользователю историю рисования
+    socket.emit('loadHistory', drawHistory);
+
+    // Принимаем ник от пользователя
+    socket.on('setNickname', (nickname) => {
+        socket.nickname = nickname; // Сохраняем ник в объекте сокета
+        console.log(`Пользователь ${nickname} установил ник`);
+    });
+
     socket.on('draw', (data) => {
-        socket.broadcast.emit('draw', data);
+        // Добавляем ник пользователя к данным рисования
+        const drawData = {
+            ...data,
+            nickname: socket.nickname || 'Аноним' // Если ник не указан, используем "Аноним"
+        };
+
+        // Сохраняем в историю
+        drawHistory.push(drawData);
+
+        // Рассылаем всем, включая отправителя
+        io.emit('draw', drawData);
     });
 
     socket.on('disconnect', () => {
-        console.log('Пользователь отключился');
+        console.log(`Пользователь ${socket.nickname || 'неизвестный'} отключился`);
     });
 });
 
